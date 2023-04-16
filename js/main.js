2ae4559c0fd82c70ee9d1e880d3e0025a92d65b7
VadimSVV <korsarishe@gmail.com>
2023-04-16 03:39:28 +0300

    Functions

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
-    $('video-player').each(function(index, videoPlayer) {
+    $('.video-player').each(function(index, videoPlayer) {
 
         //Выбор всех параметров
         let videoElement = $(videoPlayer).find('video');
-        let elePlayPauseBtn = $(videoPlayer).find('toggle-play-pause');
+        let elePlayPauseBtn = $(videoPlayer).find('.toggle-play-pause');
         let eleStartTime = $(videoPlayer).find('.start-time');
         let eleEndTime = $(videoPlayer).find('.end-time');
-        let eleVideoSeekBar = $(videoPlayer).find(.'video-seekBar');
+        let eleVideoSeekBar = $(videoPlayer).find('.video-seekBar');
         let eleVideoProgress = $(eleVideoSeekBar).find('.progress');
         let eleToggleVolume = $(videoPlayer).find('.toggle-volume');
         let eleVolumeSeekBar = $(videoPlayer).find('.volume-seekBar');
         let eleVolumeProgress = $(eleVolumeSeekBar).find('.progress');
 
         //Другие переменные
-        let totalDurationInSecond = 0;
+        let totalDurationInSeconds = 0;
         let currentTime = 0;
         let currentDuration = null;
         let totalDuration = null;
@@ -55,5 +55,99 @@ $(document).ready(function() {
         };
 
         //Обновление каждой функции
-        
-    })
+        videoElement.on('loadeddata', () => {
+            totalDurationInSeconds = videoElement['0'].duration;
+            totalDuration =helper_calculateDuration(totalDurationInSeconds);
+            updateTotalDuration();
+            updateSeekBar();
+            updateVolumeBar();
+        });
+
+        videoElement.on('timeupdate', () => {
+            currentTime = videoElement['0'].currentTime;
+            currentDuration = helper_calculateDuration(currentTime);
+            updateCurrentTime();
+            updateSeekBar();
+        });
+
+        videoElement.on('volumechange', () => {
+            volumePercentage = videoElement['0'].volume * 100;
+            updateVolumeBar();
+        });
+
+        videoElement.on('ended', () => {
+            videoElement['0'].currentTime = 0;
+            $(elePlayPauseBtn)
+            .removeClass('pause')
+            .addClass('play');
+            $(videoPlayer).removeClass('hide-controls');
+        });
+
+        //События у пользователя
+        $(elePlayPauseBtn).on('click', () => {
+            $(elePlayPauseBtn).hasClass('play') ? videoElement['0'].play() : videoElement['0'].pause();
+            $(elePlayPauseBtn).toggleClass('play pause');
+        });
+
+        $(eleToggleVolume).on('click', () => {
+            videoElement['0'].volume = videoElement['0'].volume ? 0: volumeValue;
+            $(eleToggleVolume).toggleClass('on off');
+        });
+
+        $(eleVolumeSeekBar).on('click', (e) => {
+            let tempPos = e.pageX - videoPlayer.offsetLeft - eleVolumeSeekBar['0'].offsetLeft;
+            let tempValue = tempPos / eleVolumeSeekBar['0'].clientWidth;
+
+            volumeValue = tempValue;
+            videoElement['0'].volume = tempValue.toFixed(1);
+            volumePercentage = tempValue.toFixed(1) * 100;
+            $(eleToggleVolume)
+            .addClass('on')
+            .removeClass('off');
+        });
+
+        $(eleVideoSeekBar).on('click', (e) => {
+            let tempPos = e.pageX - videoPlayer.offsetLeft - eleVideoSeekBar['0'].offsetLeft;
+            let tempValue = tempPos / eleVideoSeekBar['0'].clientWidth;
+            videoElement['0'].currentTime = tempValue * totalDurationInSeconds;
+        });
+
+    });
+
+});
+
+//Полноэкранный режим
+document.getElementsByClassName('full-screen')[0].addEventListener('click', function () {
+    if (document.fullscreenElement) {
+        document.exitFullscreen();
+    }
+    else {
+        document.getElementsByClassName('video-player')[0].requestFullscreen();
+    }
+});
+
+const helper_getPercentage = (presentTime, totalTime) => {
+    let calcPercentage = (presentTime / totalTime) * 100;
+    return parseFloat(calcPercentage.toString());
+};
+
+const helper_calculateDuration = (duration) => {
+    let seconds = parseInt(duration % 60),
+        minutes = parseInt((duration % 3600) / 60),
+        hours = parseInt(duration / 3600);
+    
+    return {
+        hours: helper_pad(hours),
+        minutes: helper_pad(minutes.toFixed()),
+        seconds: helper_pad(seconds.toFixed())
+    };
+};
+
+const helper_pad = (number) => {
+    if(number > -10 && number < 10) {
+        return '0' + number;
+    }
+    else {
+        return number;
+    }
+};
